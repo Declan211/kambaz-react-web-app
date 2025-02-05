7646968fa313cbd236abf81dfe737f52a504ded8
Declan2111 <100530857@alumnos.uc3m.es>
2025-02-04 21:35:45 -0500

    add name and github

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -19,7 +19,7 @@ export default function TOC() {
         <Nav.Link href="#/Kambaz">Kambaz</Nav.Link>
       </Nav.Item>
       <Nav.Item>
-        <Nav.Link href="https://github.com/jannunzi">My GitHub</Nav.Link>
+        <Nav.Link id="wd-github"href="https://github.com/Declan211">My GitHub</Nav.Link>
       </Nav.Item>
     </Nav>
  );}
